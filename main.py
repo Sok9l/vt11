32d9d3269e1b4b9b9572addbe0b8cd240f103505
Sok9l <93429619+Sok9l@users.noreply.github.com>
2022-06-04 08:20:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
-from random import randrange
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.skysports.com/f1/grandprix/monaco/results"
+request = requests.get(url)
+soup = BeautifulSoup(request.text, "html.parser")
+Monaco = soup.find("div",class_="page-header page-header--takeover")
+print(Monaco.text)
 
-print("Игра Камень,Ножницы,Бумагаю"
-      " Условное обозначение"
-      " 1- Камень"
-      " 2- Ножницы"
-      " 3- Бумага")
-user=int(input('Введите соответствующее число\n'))
+uri = "https://www.skysports.com/f1/grandprix/monaco/results/2022/qualifying-1"
+request = requests.get(uri)
+soup = BeautifulSoup(request.text, "html.parser")
+Q1 = soup.find("div",class_="page-filters__offset")
+print(Q1.text)
 
-if user == 1:
-    print('Ваш выбор Камень')
-if user == 2:
-    print('Ваш выбор Ножницы')
-if user == 3:
-    print('Ваш выбор Бумага')
+ura = "https://www.skysports.com/f1/grandprix/monaco/results/2022/qualifying-2"
+request = requests.get(ura)
+soup = BeautifulSoup(request.text, "html.parser")
+Q2 = soup.find("div",class_="page-filters__offset")
+print(Q2.text)
+
+urq = "https://www.skysports.com/f1/grandprix/monaco/results/2022/qualifying-3"
+request = requests.get(urq)
+soup = BeautifulSoup(request.text, "html.parser")
+Q3 = soup.find("div",class_="page-filters__offset")
+print(Q3.text)
+
+urw = "https://www.skysports.com/f1/grandprix/monaco/results/2022/race"
+request = requests.get(urw)
+soup = BeautifulSoup(request.text, "html.parser")
+Race = soup.find("div",class_="page-filters__offset")
+print(Race.text)
+
+ury = "https://www.championat.com/auto/_f1/tournament/689/standing/"
+request = requests.get(ury)
+soup = BeautifulSoup(request.text, "html.parser")
+DriverStandings= soup.find("dib",class_="js-tournament-tabs eane tournament-tabs")
+print(DriverStandings.text)
 
 
 
-comp = randrange(3) + 1
-if comp == 1:
-    print('Выбор компа Камень\n')
-if comp == 2:
-    print('Выбор компа Ножницы\n')
-if comp == 3:
-    print('Выбор компа Бумага\n')
 
-if comp == user:
-    print('Произошла ничья!!!!\n')
-elif comp == 1 and user == 2 or comp == 2 and user == 3 or comp == 3 and user == 1:
-    print('Комп выиграл(\n)')
-elif comp == 2 and user == 1 or comp == 3 and user == 2 or comp == 1  and user == 3:
-    print('Победил ты(\n)')
 
 
 
